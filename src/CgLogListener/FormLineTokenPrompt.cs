875e27bc14fada6ad5ae563cd9a8664e1aa93194
windofnet <windofnet@hotmail.com>
2022-08-18 15:13:53 +0800

    add feat: line notify

---
+++ src/CgLogListener/FormLineTokenPrompt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CgLogListener
+{
+    public partial class FormLineTokenPrompt : Form
+    {
+        FormLineTokenPrompt()
+        {
+            InitializeComponent();
+            this.ImeMode = ImeMode.OnHalf;
+        }
+
+        public static DialogResult ShowDialog(IWin32Window owner, out string value)
+        {
+            FormLineTokenPrompt f = new FormLineTokenPrompt();
+            DialogResult result = f.ShowDialog(owner);
+            value = f.txtValue.Text;
+
+            return result;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}
---
+++ src/CgLogListener/LineNotifyHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CgLogListener
+{
+    public static class LineNotifyHelper
+    {
+        public static async Task Notify(string token, string message)
+        {
+            using (var lineHttpClient = new HttpClient())
+            {
+                lineHttpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+                var httpContent = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    { "message", message }
+                });
+
+                var p = await lineHttpClient.PostAsync("https://notify-api.line.me/api/notify", httpContent);
+                p.EnsureSuccessStatusCode();
+            }
+        }
+    }
+}
